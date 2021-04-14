bc588db2d3c7bf6b2b378b7cc71e10c554a95304
Meshari <alh3rbi@gmail.com>
2021-04-14 17:27:43 -0600

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const url = 'https://randomuser.me/api/'
+  const [user, setUser] = useState(null)
+  const [person, setPerson] = useState('')
+
+  const fetchData = async () => {
+    const data = await fetch(url).then((res) => res.json())
+
+    const single = data.results[0]
+    const {
+      gender,
+      name: { first, last },
+      picture: { large: image },
+    } = single
+
+    const newPerson = {
+      name: `${first} ${last}`,
+      gender,
+      image,
+    }
+
+    setPerson(newPerson)
+    setUser(data.results)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(person)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <div className='container'>
+        <img src={person.image} alt={person.name} />
+        <h2>{person.name}</h2>
+        <p>{person.gender}</p>
+      </div>
+      <h3>Person</h3>
+      <pre className=''>{JSON.stringify(person, undefined, 2)}</pre>
+      <hr style={{ margin: '10px' }} />
+      <h3>All</h3>
+
+      <pre className=''>{JSON.stringify(user, undefined, 2)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
